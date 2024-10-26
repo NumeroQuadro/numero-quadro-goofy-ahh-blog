f498ce8d4d12934ec16d02545c321d9a00913578
numero_quadro <dimabelunin777@niuitmo.ru>
2024-10-26 17:21:08 +0300

    tab switcher done. simple footer added

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
+const header = document.getElementById('header');
+const briefSection = document.getElementById('brief_section');
+const meSection = document.getElementById('me_section');
+const musicSection = document.getElementById('music_section');
+const freeTimeSection = document.getElementById('free_time_section');
+
+const firstTabPixelNumber = (briefSection.clientHeight) * 0.7;
+const secondTabPixelNumber = (briefSection.clientHeight + meSection.clientHeight - header.clientHeight);
+const thirdTabPixelNumber = (briefSection.clientHeight + meSection.clientHeight + musicSection.clientHeight - header.clientHeight);
+
+const aboutMeDiv = document.getElementById('me_tab');
+const musicDiv = document.getElementById('music_tab');
+const freeTimeDiv = document.getElementById('free_time_tab');
+const briefDiv = document.getElementById('brief_section')
+
+let tabs = new Set();
+
 window.onscroll = function() {
   activeTabsSwitcher();
 };
 
 function activeTabsSwitcher() {
-  const aboutMeDiv = document.getElementById('me_section');
-  const musicDiv = document.getElementById('music_section');
-  const freeTimeDiv = document.getElementById('free_time_section');
-
   const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
+  console.log("size of set: " + tabs.size)
+
   if (aboutMeDiv && musicDiv && freeTimeDiv) {
-    if (scrollTop < 100) { // brief
-      updateDivClasses(aboutMeDiv, musicDiv, freeTimeDiv, "brief");
-    } else if (scrollTop >= 100 && scrollTop < 200) { // about me
-      updateDivClasses(aboutMeDiv, musicDiv, freeTimeDiv, "aboutMe");
-    } else if (scrollTop >= 200 && scrollTop < 300) { // music
-      updateDivClasses(aboutMeDiv, musicDiv, freeTimeDiv, "music");
-    } else if (scrollTop >= 300) { // free time
-      updateDivClasses(aboutMeDiv, musicDiv, freeTimeDiv, "freeTime");
+    if (scrollTop < firstTabPixelNumber) { // brief
+      currentTab = briefDiv;
+      updateDivClasses(currentTab);
+    } else if (scrollTop >= firstTabPixelNumber && scrollTop < secondTabPixelNumber) { // about me
+      currentTab = aboutMeDiv;
+      updateDivClasses(currentTab);
+    } else if (scrollTop >= secondTabPixelNumber && scrollTop < thirdTabPixelNumber) { // music
+      currentTab = musicDiv;
+      updateDivClasses(currentTab);
+    } else if (scrollTop >= thirdTabPixelNumber) { // free time
+      currentTab = freeTimeDiv;
+      updateDivClasses(currentTab);
     }
   }
 }
 
-function updateDivClasses(aboutMeDiv, musicDiv, freeTimeDiv, section) {
-  if (section === "brief") {
-    // Reset all sections to inactive
-    aboutMeDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-lavender');
-    aboutMeDiv.classList.add('content__rectangle--inactive');
+function switchTabs(fromTab, toTab) {
+  console.log('toTab is: ' + toTab);
+  console.log('fromTab is: ' + fromTab);
 
-    musicDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-yellow');
-    musicDiv.classList.add('content__rectangle--inactive');
-
-    freeTimeDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-cyan');
-    freeTimeDiv.classList.add('content__rectangle--inactive');
+  if (toTab != briefDiv) {
+    toTab.classList.add('content__rectangle--active');
+    toTab.classList.remove('content__rectangle--inactive');
   }
 
-  if (section === "aboutMe") {
-    // Activate aboutMe section
-    aboutMeDiv.classList.add('content__rectangle--active', 'content__rectangle--border-lavender');
-    aboutMeDiv.classList.remove('content__rectangle--inactive');
+  if (fromTab != briefDiv) {
+    fromTab.classList.remove('content__rectangle--active')
+    fromTab.classList.add('content__rectangle--inactive')
+  }   
+}
 
-    musicDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-yellow');
-    musicDiv.classList.add('content__rectangle--inactive');
-
-    freeTimeDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-cyan');
-    freeTimeDiv.classList.add('content__rectangle--inactive');
+function updateDivClasses(currentTab) {
+  console.log('i am on updateDivClasses function!');
+  if (tabs.size < 2) {
+    tabs.add(currentTab);
   }
 
-  if (section === "music") {
-    // Activate music section
-    musicDiv.classList.add('content__rectangle--active', 'content__rectangle--border-yellow');
-    musicDiv.classList.remove('content__rectangle--inactive');
-
-    aboutMeDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-lavender');
-    aboutMeDiv.classList.add('content__rectangle--inactive');
-
-    freeTimeDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-cyan');
-    freeTimeDiv.classList.add('content__rectangle--inactive');
-  }
-
-  if (section === "freeTime") {
-    // Activate freeTime section
-    freeTimeDiv.classList.add('content__rectangle--active', 'content__rectangle--border-cyan');
-    freeTimeDiv.classList.remove('content__rectangle--inactive');
-
-    aboutMeDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-lavender');
-    aboutMeDiv.classList.add('content__rectangle--inactive');
-
-    musicDiv.classList.remove('content__rectangle--active', 'content__rectangle--border-yellow');
-    musicDiv.classList.add('content__rectangle--inactive');
+  if (tabs.size === 2) {
+    console.log(tabs);
+    
+    let [fromTab, toTab] = tabs;
+    tabs.delete(fromTab);
+    tabs.delete(toTab);
+    switchTabs(fromTab, toTab);
   }
 }
