261a9ef65b2df6f6c15cdfc82a9ef32be128764d
numero_quadro <dimabelunin777@niuitmo.ru>
2024-11-09 23:56:12 +0300

    sidebar and content are friendly

--- script.js
+++ script.js
@@ -1,84 +1,37 @@
-const header = document.getElementById('header');
-const briefSection = document.getElementById('brief_section');
-const meSection = document.getElementById('me_section');
-const musicSection = document.getElementById('music_section');
-const freeTimeSection = document.getElementById('free_time_section');
+var track_1 = document.getElementById("track_1");
+var track_2 = document.getElementById("track_2");
+var track_3 = document.getElementById("track_3");
+var track_4 = document.getElementById("track_4");
+var track_5 = document.getElementById("track_5");
+let tracks = [track_1, track_2, track_3, track_4, track_5]
 
-const firstTabPixelNumber = (briefSection.clientHeight) * 0.7;
-const secondTabPixelNumber = (briefSection.clientHeight + meSection.clientHeight - header.clientHeight);
-const thirdTabPixelNumber = (briefSection.clientHeight + meSection.clientHeight + musicSection.clientHeight - header.clientHeight);
+var description_1 = document.getElementById("description_1");
+var description_2 = document.getElementById("description_2");
+var description_3 = document.getElementById("description_3");
+var description_4 = document.getElementById("description_4");
+var description_5 = document.getElementById("description_5");
+let descriptions = [description_1, description_2, description_3, description_4, description_5]
 
-const aboutMeDiv = document.getElementById('me_tab');
-const musicDiv = document.getElementById('music_tab');
-const freeTimeDiv = document.getElementById('free_time_tab');
-const briefDiv = document.getElementById('brief_section')
-
-let tabs = new Set();
-
-window.onscroll = function() {
-  activeTabsSwitcher();
-};
-
-function activeTabsSwitcher() {
-  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-  console.log("size of set: " + tabs.size)
-
-  if (aboutMeDiv && musicDiv && freeTimeDiv) {
-    if (scrollTop < firstTabPixelNumber) { // brief
-      currentTab = briefDiv;
-      updateDivClasses(currentTab);
-    } else if (scrollTop >= firstTabPixelNumber && scrollTop < secondTabPixelNumber) { // about me
-      currentTab = aboutMeDiv;
-      updateDivClasses(currentTab);
-    } else if (scrollTop >= secondTabPixelNumber && scrollTop < thirdTabPixelNumber) { // music
-      currentTab = musicDiv;
-      updateDivClasses(currentTab);
-    } else if (scrollTop >= thirdTabPixelNumber) { // free time
-      currentTab = freeTimeDiv;
-      updateDivClasses(currentTab);
-    }
-  }
+function revealDescription(descriptionId) {
+  console.log('revealing', descriptionId);
+  let description = document.getElementById(descriptionId);
+  description.classList.add("visible");
 }
 
-function switchTabs(fromTab, toTab) {
-  console.log('toTab is: ' + toTab);
-  console.log('fromTab is: ' + fromTab);
-
-  if (toTab != briefDiv) {
-    toTab.classList.add('content__rectangle--active');
-    toTab.classList.remove('content__rectangle--inactive');
-  }
-
-  if (fromTab != briefDiv) {
-    fromTab.classList.remove('content__rectangle--active')
-    fromTab.classList.add('content__rectangle--inactive')
-  }   
+function hideDescription(descriptionId) {
+  console.log('hiding', descriptionId);
+  let description = document.getElementById(descriptionId);
+  description.classList.remove("visible");
 }
 
-function updateDivClasses(currentTab) {
-  console.log('i am on updateDivClasses function!');
-  if (tabs.size < 2) {
-    tabs.add(currentTab);
-  }
-
-  if (tabs.size === 2) {
-    console.log(tabs);
-    
-    let [fromTab, toTab] = tabs;
-    tabs.delete(fromTab);
-    tabs.delete(toTab);
-    switchTabs(fromTab, toTab);
-  }
+function revealAuthor(quoteId) {
+  console.log('revealing quote author');
+  let quote = document.getElementById(quoteId);
+  quote.classList.add("visible");
 }
 
-(function () {
-  window.addEventListener('load', function () {
-      const loadTime = (performance.now() / 1000).toFixed(3);
-
-      const loadTimeText = document.getElementById('load-time-text');
-      if (loadTimeText) {
-          loadTimeText.textContent = `Page load time is ${loadTime} Seconds`;
-      }
-  });
-})();
+function hideAuthor(quoteId) {
+  console.log('revealing quote author');
+  let quote = document.getElementById(quoteId);
+  quote.classList.remove("visible");
+}
